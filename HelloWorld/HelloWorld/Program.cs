26abc6cb440c57cf3a0acc36d6c0fe43448d5ea3
Louwrens <louwrenskoltz@gmail.com>
2020-08-09 18:15:31 +0200

    Update a comment line 19

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
         {
             //The Global variable is assigned "Hello World!"
             //Thus being access by a Method or any other anywhere the class.
-            //The Method is instansiated and thus allows "Hello World!" to be printed.
+            //The Method is instansiated in main method and thus allows "Hello World!" to be printed.
 
             //This will add to Git Status and commit as new
 
