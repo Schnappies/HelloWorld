49661859f2ffc70364b8a664df9944a80bc261b1
Louwrens <louwrenskoltz@gmail.com>
2020-08-09 17:08:47 +0200

    Add HelloWorld Files

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        public static string result;
+
+        public static string doSomething()
+        {
+            result = "Hello World!";
+            return "Something";
+        }
+        static void Main(string[] args)
+        {
+            //The Global variable is assigned "Hello World!"
+            //Thus being access by a Method or any other anywhere the class.
+            //The Method is instansiated and thus allows "Hello World!" to be printed.
+
+            doSomething();
+            Console.WriteLine(result);
+        }
+    }
+}
