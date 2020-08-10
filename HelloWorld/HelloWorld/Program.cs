f5123b51cae781f33bd573ca7dd59ff43f247b3d
Louwrens <louwrenskoltz@gmail.com>
2020-08-10 15:34:28 +0200

    Return to basic program

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,6 +2,11 @@
 
 namespace HelloWorld
 {
+
+    //The Global variable is assigned "Hello World!"
+    //Thus being access by a Method or any other anywhere the class.
+    //The Method is instansiated in main method and thus allows "Hello World!" to be printed.
+
     class Program
     {
 
@@ -13,21 +18,10 @@ namespace HelloWorld
             return "Something";
         }
 
-        public static string doSomethingAgain()
-        {
-            result = "Hello World! Printing Again";
-            return "Something two";
-        }
         static void Main(string[] args)
         {
-            //The Global variable is assigned "Hello World!"
-            //Thus being access by a Method or any other anywhere the class.
-            //The Method is instansiated in main method and thus allows "Hello World!" to be printed.
-
-            //This will add to Git Status and commit as new
 
             doSomething();
-            doSomethingAgain();
             Console.WriteLine(result);
         }
     }
